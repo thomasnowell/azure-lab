e9a58eb49ea0797062975a7cf0726781d53db436
thomasnowell <thomasolivernowell@gmail.com>
2023-02-16 15:20:43 +0100

    added app service and plan. actually saved

--- terraform/main.tf
+++ terraform/main.tf
@@ -12,10 +12,32 @@ provider "azurerm" {
 }
 
 locals {
-  resource_group_name = "rg-${var.environment}-${var.location}-${var.name}"
+  resource_group_name   = "rg-${var.environment}-${var.location}-${var.name}"
+  app_service_plan_name = "asp-${var.environment}-${var.location}-${var.name}"
+  app_service_name      = "wa-${var.environment}-${var.location}-${var.name}"
 }
 
 resource "azurerm_resource_group" "this" {
   name     = local.resource_group_name
   location = var.location_long
+}
+resource "azurerm_service_plan" "this" {
+  name                = local.app_service_plan_name
+  resource_group_name = azurerm_resource_group.this.name
+  location            = azurerm_resource_group.this.location
+  os_type             = "Linux"
+  sku_name            = "B1"
+}
+
+resource "azurerm_linux_web_app" "this" {
+  name                = local.app_service_name
+  resource_group_name = azurerm_resource_group.this.name
+  location            = azurerm_resource_group.this.location
+  service_plan_id     = azurerm_service_plan.this.id
+
+  site_config {
+    application_stack {
+      node_version = "16-lts"
+    }
+  }
 }
